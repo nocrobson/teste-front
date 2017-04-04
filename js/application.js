b40217b7d75f56441c56172eccf4287743ae787d
ORobson <noc.robson@gmail.com>
2017-04-03 23:03:21 -0300

    first commit

---
+++ js/application.js
@@ -0,0 +1,9 @@
+$('.datepicker').pickadate({
+	selectMonths: true, // Creates a dropdown to control month
+	selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
+$(document).ready(function() {
+	$('select').material_select();
+});
+
